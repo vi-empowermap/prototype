52db937f9b934ca0ab329a183238f4ad1fb08b27
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-09 13:34:56 +0200

    update

---
+++ src/app/(home)/components/Wrapper.js
@@ -0,0 +1,34 @@
+"use client"
+/* CSR: NO SSR */
+
+
+
+const Wrapper = () => {
+  return (
+    <main className="flex w-screen h-screen bg-slate-300">
+      <div className="flex flex-col flex-1 bg-red-400 h-full">
+        <nav className="w-full bg-green-400 h-36 flex">
+          <h1 className="bg-blue-300 text-4xl md:text-6xl lg:text-7xl font-bold flex items-center px-4 border-r-2 border-black">
+            <span>EMPOWER MAP</span>
+          </h1>
+          <div className="flex flex-col text-2xl font-semibold bg-blue-300 flex-grow">
+            <div className="flex-1 w-full flex items-center border-b-2 border-black px-4">
+              <span>Suchen</span>
+            </div>
+            <div className="flex-1 w-full flex items-center px-4">
+              <span>Filtern</span>
+            </div>
+          </div>
+        </nav>
+        <div className="flex-1 bg-violet-500 flex justify-center items-center">
+          <div>Map</div>
+        </div>
+      </div>
+      <div className="w-[calc(350px+4vw)] h-full border-l-2 border-black">
+        <div>List</div>
+      </div>
+    </main>
+  );
+};
+
+export default Wrapper;
--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,26 +1,12 @@
+import { Suspense } from "react";
+import Wrapper from "./components/Wrapper";
 
-export default function Home() {
+export default async function Home() {
+  // Loading page testing
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   return (
-    <main className="flex w-screen h-screen bg-slate-300">
-        <div className="flex flex-col flex-1 bg-red-400 h-full">
-          <nav className="w-full bg-green-400 h-36 flex">
-            <h1 className="bg-blue-300 text-8xl font-bold flex items-center px-4 border-r-2 border-black"><span>EMPOWER MAP</span></h1>
-            <div className="flex flex-col text-2xl font-semibold bg-blue-300 flex-grow">
-              <div className="flex-1 w-full flex items-center border-b-2 border-black px-4">
-                <span>Suchen</span>
-              </div>
-              <div className="flex-1 w-full flex items-center px-4">
-                <span>Filtern</span>
-              </div>
-            </div>
-          </nav>
-          <div className="flex-1 bg-violet-500 flex justify-center items-center">
-              <div>Map</div>
-          </div>
-        </div>
-        <div className="w-[600px] h-full">
-          <div>List</div>
-        </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Wrapper />
+    </Suspense>
   );
 }
