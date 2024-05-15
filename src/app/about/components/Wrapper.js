692fea092373a9561d08e9ca31d655328b14d56c
dain <57316771+DainPark-web@users.noreply.github.com>
2024-05-15 18:04:01 +0200

    update

---
+++ src/app/about/components/Wrapper.js
@@ -0,0 +1,19 @@
+import MenuNav from "@/app/components/MenuNav";
+
+const Wrapper = ({
+  kirbyPanelData: {
+    result: { content: data },
+  },
+}) => {
+  console.log(data);
+  return (
+    <div className="w-screen min-h-screen h-full">
+      <MenuNav />
+      <div className="w-full h-fit bg-blue-300 px-2">
+        <div className="text-3xl font-medium">{data.about_title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Wrapper;
