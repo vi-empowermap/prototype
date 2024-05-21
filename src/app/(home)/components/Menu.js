a26705fafbb3e814d6b056bdd861c41bc0dc9933
dain <57316771+DainPark-web@users.noreply.github.com>
2024-05-21 10:56:00 +0200

    update

--- src/app/(home)/components/Menu.js
+++ src/app/(home)/components/Menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
     }
   return (
     <div className="h-full px-2 flex justify-center items-center cursor-pointer hover:bg-black hover:text-white transition-all relative z-[1300] group">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-8 h-8">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 lg:w-8 lg:h-8">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
       </svg>
       <div className="hidden group-hover:flex flex-col absolute top-full left-0 h-fit bg-white border-y-2 lg:border-2 border-black text-black w-screen lg:w-fit">
