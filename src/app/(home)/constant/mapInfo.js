e43fbf7028151c8951f59ff46a0917dbabd043de
dain <57316771+DainPark-web@users.noreply.github.com>
2024-05-24 12:45:45 +0200

    update

--- src/app/(home)/constant/mapInfo.js
+++ src/app/(home)/constant/mapInfo.js
@@ -1,7 +1,8 @@
 export const MAPTILELAYER = {
-    ex01: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    ex01: "https://api.mapbox.com/styles/v1/cmmm/clwkjojsa00sa01pndps96fc6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY21tbSIsImEiOiJjbDBxb2pzdmMxZWw0M2pudGNlemc1eXVnIn0.TqiRguWLr3yu-8FGqahc-A",
+    ex03: "https://api.mapbox.com/styles/v1/vis-int/ck3eo5das52db1ckkq9sndedt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidmlzLWludCIsImEiOiJjanI2NWppYTIwNGR2NDRwaGI1N2xzZXBxIn0.dmZ6dkYdM95wixqH0mQb_w",
+    // ex01: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     ex02: "https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.png",
-    // ex03: "https://api.mapbox.com/styles/v1/vis-int/ck3eo5das52db1ckkq9sndedt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidmlzLWludCIsImEiOiJjanI2NWppYTIwNGR2NDRwaGI1N2xzZXBxIn0.dmZ6dkYdM95wixqH0mQb_w",
     exMini01: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     exMini02: ""
-}
+}       
