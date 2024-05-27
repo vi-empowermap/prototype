144349268d193577013bad0cf2e2b5dad4dbe3af
dain <57316771+DainPark-web@users.noreply.github.com>
2024-05-27 15:54:13 +0200

    update

--- src/app/(home)/components/Menu.js
+++ src/app/(home)/components/Menu.js
@@ -12,6 +12,7 @@ const Menu = () => {
       </svg>
       <div className="hidden group-hover:flex flex-col absolute top-full left-0 h-fit bg-white border-y lg:border border-black text-black w-screen lg:w-fit">
         <div onClick={() => onPush("/about")} className="font-medium text-xl border-b border-black px-2 py-2 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">About & Contact</div>
+        <div onClick={() => onPush("/signup")} className="font-medium text-xl border-b border-black px-2 py-2 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">My Organisation</div>
         <div className="font-medium text-xl border-b border-black px-2 py-2 last:border-b-0 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">Imprint</div>
         <div className="font-medium text-xl border-b border-black px-2 py-2 last:border-b-0 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">FAQ</div>
       </div>
