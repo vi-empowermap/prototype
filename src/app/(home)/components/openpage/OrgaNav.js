c8cbf9fd3a863a5326aa1c9beb6e13c644548395
dain <57316771+DainPark-web@users.noreply.github.com>
2024-08-02 17:10:02 +0200

    test Icon stroke

--- src/app/(home)/components/openpage/OrgaNav.js
+++ src/app/(home)/components/openpage/OrgaNav.js
@@ -1,3 +1,7 @@
+import { ICON_SIZE, ICON_STROKE_SIZE } from "../../constant/iconSize";
+import Close from "/public/assets/icons/close.svg"
+import Copy from "/public/assets/icons/copy.svg"
+
 const OrgaNav = ({onClose}) => {
     const onCopyText = () => {
         navigator.clipboard.writeText(`${window.location.href}`);
@@ -5,20 +9,14 @@ const OrgaNav = ({onClose}) => {
       };
     return (
         <nav className="flex gap-4 justify-end p-4 lg:mb-2">
+          
           <div onClick={onCopyText} className="cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 lg:w-8 lg:h-8">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
-              />
-            </svg>
+            <Copy style={{strokeWidth: ICON_STROKE_SIZE,stroke: "#000",width: ICON_SIZE, height: ICON_SIZE}} />
           </div>
           <div onClick={onClose} className="cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 lg:w-8 lg:h-8">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
+            <Close style={{strokeWidth: ICON_STROKE_SIZE, stroke: "#000",width: ICON_SIZE, height: ICON_SIZE}} />
           </div>
+          
         </nav>
     )
 }
---
+++ src/app/(home)/constant/iconSize.js
@@ -0,0 +1,2 @@
+export const ICON_SIZE = "35px"
+export const ICON_STROKE_SIZE = "30px"
