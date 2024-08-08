b6ac9229de8b7b785d515a3eea6cf56e14c540af
dain <57316771+DainPark-web@users.noreply.github.com>
2024-08-08 16:14:38 +0200

    update: Marker Icons

--- src/app/utils/hooks/useMarkerIcon.js
+++ src/app/utils/hooks/useMarkerIcon.js
@@ -1,198 +1,284 @@
 export const useMarkerIcon = ({ id, archivoraktiv, activeColor, artderorganisation, selection }) => {
   // Verin : Circle
   const aI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .a${id}1 {
-        fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <rect class="a${id}1" x="0" y="0" width="800" height="800" rx="400" ry="400"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <title>Art der Organisation: Verein, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+    <desc>A custom marker for the map</desc>
+    <defs>
+      <style>
+        .cls-1{fill:#fff;}.cls-1,
+        .cls-2{stroke-width:0px;}
+        .a${id}1{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+      </style>
+    </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <circle class="a${id}1" cx="16" cy="16" r="12"/>
   </svg>`;
-  // Initiative : Rect
+  // Initiative : Circle
   const bI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <title>Art der Organisation: Initiative, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+    <desc>A custom marker for the map</desc>
     <defs>
       <style>
-        .a${id}2 {
-          fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-          stroke-width: 0px;
-        }
+        .cls-1{fill:#fff;}
+        .cls-1,.cls-2{stroke-width:0px;}
+        .a${id}2{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
       </style>
     </defs>
-    <rect class="a${id}2" x="0" y="0" width="800" height="800"/>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <circle class="a${id}2" cx="16" cy="16" r="12"/>
   </svg>`;
-  // Ngo : Triangle
+  // Ngo : Polygon
   const cI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-    <defs>
-      <style>
-        .a${id}3 {
-          fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-          stroke-width: 0px;
-        }
-      </style>
-    </defs>
-    <polygon class="a${id}3" points="400 42.6 .7 734.2 799.3 734.2 400 42.6"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Ngo, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}3{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <polygon class="a${id}3" points="5.61 10 5.61 22 16 28 26.39 22 26.39 10 16 4 5.61 10"/>
   </svg>`;
-  // Projectgrouppe
+  // Projectgrouppe : Polygon
   const dI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .a${id}4 {
-        fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <polygon class="a${id}4" points="400 19.6 494.4 310.2 800 310.2 552.8 489.8 647.2 780.4 400 600.8 152.8 780.4 247.2 489.8 0 310.2 305.6 310.2 400 19.6"/>
-</svg>`;
-  // Bildungseinrichtung
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Projectgrouppe, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}4{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <polygon class="a${id}4" points="5.61 10 5.61 22 16 28 26.39 22 26.39 10 16 4 5.61 10"/>
+  </svg>`;
+  // Bildungseinrichtung : Rect
   const eI = `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-<title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .a${id}5 {
-        fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <polygon class="a${id}5" points="599.3 54 199.8 54 0 400 199.8 746 599.3 746 799.1 400 599.3 54"/>
-</svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Bildungseinrichtung, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}5{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <rect class="a${id}5" x="6" y="6" width="20" height="20"/>
+    </svg>
 `;
-  // Kultureinrichtung
+  // Kultureinrichtung : Rect
   const fI = `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-<title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .a${id}6 {
-        fill: ${archivoraktiv === "aktive" ? "black" : "gray"};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <path class="a${id}6" d="M800,530.5c0,116-94.1,210.1-210.1,210.1s-156.2-49.1-189.9-120.2c-33.7,71-106.1,120.2-189.9,120.2S0,646.5,0,530.5s92.8-208.8,207.8-210.1c-4.1-16.3-6.2-33.4-6.2-50.9,0-116,94.1-210.1,210.1-210.1s210.1,94.1,210.1,210.1-2.3,35.7-6.6,52.4c104.1,12.5,184.8,101.1,184.8,208.6Z"/>
-</svg>`;
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Kultureinrichtung, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}6{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <rect class="a${id}6" x="6" y="6" width="20" height="20"/>
+    </svg>
+    `;
+  // Verbund/Verband : Triangle
+  const gI = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Verbund/Verband, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}7{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+        <circle class="cls-1" cx="16" cy="16" r="16"/>
+        <polygon class="a${id}7" points="16 5.47 5 24.53 27 24.53 16 5.47"/>
+    </svg>
+    `;
+  // Kollektiv : Triangle
+  const hI = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Kollektiv, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}8{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+        <circle class="cls-1" cx="16" cy="16" r="16"/>
+        <polygon class="a${id}8" points="16 5.47 5 24.53 27 24.53 16 5.47"/>
+    </svg>
+    `;
+  // Weitere : rotated Triangle 
+  const iI = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Weitere, status: ${archivoraktiv === "aktive" ? "aktiv" : "inaktiv"}</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .a${id}9{fill: ${archivoraktiv === "aktive" ? "#000" : "#BFBFBF"};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <polygon class="a${id}9" points="16 27.88 27 8.82 5 8.82 16 27.88"/>
+    </svg>
+    `;
 
   /* Select Mode */
-  // Verion
+  // Verin : Circle
   const saI = `
-  <svg  xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .b${id}1 {
-        fill: ${activeColor};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  
-  <rect class="b${id}1" x="0" y="0" width="800" height="800" rx="400" ry="400"/>
-</svg>`;
-  // Initiative
-  const sbI = `
-  <svg  xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <title>Art der Organisation: Verein, status: Selected</title>
+    <desc>A custom marker for the map</desc>
     <defs>
       <style>
-        .b${id}2 {
-          fill: ${activeColor};
-          stroke-width: 0px;
-        }
+        .cls-1{fill:#fff;}.cls-1,
+        .cls-2{stroke-width:0px;}
+        .b${id}1{fill: ${activeColor};}
       </style>
     </defs>
-    <rect class="b${id}2" x="0" y="0" width="800" height="800"/>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <circle class="b${id}1" cx="16" cy="16" r="12"/>
+  </svg>
+  `;
+  // Initiative : Circle
+  const sbI = `
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <title>Art der Organisation: Initiative, status: Selected</title>
+    <desc>A custom marker for the map</desc>
+    <defs>
+      <style>
+        .cls-1{fill:#fff;}.cls-1,
+        .cls-2{stroke-width:0px;}
+        .b${id}2{fill: ${activeColor};}
+      </style>
+    </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <circle class="b${id}2" cx="16" cy="16" r="12"/>
   </svg>`;
-  // Ngo
+  // Ngo : Polygon
   const scI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1200 1200">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .b${id}3 {
-        fill: ${activeColor};
-        stroke-width: 0px;
-      }
-
-      .b${id}32 {
-        fill: none;
-        stroke: ${activeColor};
-        stroke-miterlimit: 10;
-        stroke-width: 21px;
-      }
-    </style>
-  </defs>
-  <polygon class="b${id}3" points="600 158.6 200.7 850.2 999.3 850.2 600 158.6"/>
-  <circle class="b${id}32" cx="600" cy="600" r="520.1"/>
-</svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Ngo, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}3{fill: ${activeColor};}
+        </style>
+      </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <polygon class="b${id}3" points="5.61 10 5.61 22 16 28 26.39 22 26.39 10 16 4 5.61 10"/>
+  </svg>
  `;
-  // Projectgrouppe
+  // Projectgrouppe : Polygon
   const sdI = `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-  <title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .b${id}4 {
-        fill: ${activeColor};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <polygon class="b${id}4" points="400 19.6 494.4 310.2 800 310.2 552.8 489.8 647.2 780.4 400 600.8 152.8 780.4 247.2 489.8 0 310.2 305.6 310.2 400 19.6"/>
-</svg>`;
-  // Bildungseinrichtung
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Projectgrouppe, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}4{fill: ${activeColor};}
+        </style>
+      </defs>
+    <circle class="cls-1" cx="16" cy="16" r="16"/>
+    <polygon class="b${id}4" points="5.61 10 5.61 22 16 28 26.39 22 26.39 10 16 4 5.61 10"/>
+  </svg>`;
+  // Bildungseinrichtung : Rect
   const seI = `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-<title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .b${id}5 {
-        fill: ${activeColor};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <polygon class="b${id}5" points="599.3 54 199.8 54 0 400 199.8 746 599.3 746 799.1 400 599.3 54"/>
-</svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Bildungseinrichtung, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}5{fill: ${activeColor};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <rect class="b${id}5" x="6" y="6" width="20" height="20"/>
+    </svg>  
 `;
-  // Kultureinrichtung
+  // Kultureinrichtung : Rect
   const sfI = `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-<title>Custom Marker</title>
-            <desc>A custom marker for the map</desc>
-  <defs>
-    <style>
-      .b${id}6 {
-        fill: ${activeColor};
-        stroke-width: 0px;
-      }
-    </style>
-  </defs>
-  <path class="b${id}6" d="M800,530.5c0,116-94.1,210.1-210.1,210.1s-156.2-49.1-189.9-120.2c-33.7,71-106.1,120.2-189.9,120.2S0,646.5,0,530.5s92.8-208.8,207.8-210.1c-4.1-16.3-6.2-33.4-6.2-50.9,0-116,94.1-210.1,210.1-210.1s210.1,94.1,210.1,210.1-2.3,35.7-6.6,52.4c104.1,12.5,184.8,101.1,184.8,208.6Z"/>
-</svg>`;
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Kultureinrichtung, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}6{fill: ${activeColor};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <rect class="b${id}6" x="6" y="6" width="20" height="20"/>
+    </svg> `;
+  // Verbund/Verband : Triangle
+  const sgI = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Verbund/Verband, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}7{fill: ${activeColor};}
+        </style>
+      </defs>
+        <circle class="cls-1" cx="16" cy="16" r="16"/>
+        <polygon class="b${id}7" points="16 5.47 5 24.53 27 24.53 16 5.47"/>
+    </svg>`;
+  // Kollektiv : Triangle
+  const shI = `
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Kollektiv, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}8{fill: ${activeColor};}
+        </style>
+      </defs>
+        <circle class="cls-1" cx="16" cy="16" r="16"/>
+        <polygon class="b${id}8" points="16 5.47 5 24.53 27 24.53 16 5.47"/>
+    </svg>`;
+  // Weitere : rotated Triangle
+  const siI = `
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <title>Art der Organisation: Weitere, status: Selected</title>
+      <desc>A custom marker for the map</desc>
+      <defs>
+        <style>
+          .cls-1{fill:#fff;}
+          .cls-1,.cls-2{stroke-width:0px;}
+          .b${id}9{fill: ${activeColor};}
+        </style>
+      </defs>
+      <circle class="cls-1" cx="16" cy="16" r="16"/>
+      <polygon class="b${id}9" points="16 27.88 27 8.82 5 8.82 16 27.88"/>
+    </svg>`;
 
   if (selection) {
     switch (artderorganisation) {
@@ -206,8 +292,14 @@ export const useMarkerIcon = ({ id, archivoraktiv, activeColor, artderorganisati
         return sdI;
       case "e":
         return seI;
-      default:
+      case "f": 
         return sfI;
+      case "g": 
+        return sgI;
+      case "h":
+        return shI;
+      default:
+        return siI;
     }
   } else {
     switch (artderorganisation) {
@@ -221,8 +313,14 @@ export const useMarkerIcon = ({ id, archivoraktiv, activeColor, artderorganisati
         return dI;
       case "e":
         return eI;
-      default:
+      case "f": 
         return fI;
+      case "g": 
+        return gI;
+      case "h":
+        return hI;
+      default:
+        return iI;
     }
   }
 };
