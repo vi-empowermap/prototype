27ba6838df0142da6b338cc8fdffb3fd1b3e1f32
dain <57316771+DainPark-web@users.noreply.github.com>
2024-08-09 11:32:13 +0200

    lightHouse: 95 100 96 100

--- src/app/(home)/components/Menu.js
+++ src/app/(home)/components/Menu.js
@@ -1,16 +1,15 @@
 import { useRouter } from "next/navigation";
 import Translator from "./menu/Translator";
-
+import MenuIcon from "/public/assets/icons/menu.svg"
+import { ICON_SIZE, ICON_STROKE_SIZE } from "../constant/iconSize";
 const Menu = () => {
     const router = useRouter();
     const onPush = (path) => {
         router.push(path)
     }
   return (
-    <div className="h-full px-2 flex justify-center items-center cursor-pointer hover:bg-black hover:text-white transition-all relative z-[1300] group select-none font-jetBrainsMono font-medium border-r border-black">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 lg:w-8 lg:h-8">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-      </svg>
+    <div className="h-full px-2 flex justify-center items-center cursor-pointer hover:bg-black hover:text-white transition-all relative z-[1300] group select-none font-jetBrainsMono font-medium border-r border-black stroke-black hover:stroke-white">
+      <MenuIcon style={{strokeWidth: ICON_STROKE_SIZE,width: ICON_SIZE, height: ICON_SIZE}} />
       <div className="hidden group-hover:flex flex-col absolute top-full left-0 h-fit bg-white border-y lg:border border-black text-black w-screen lg:w-fit">
         <div onClick={() => onPush("/about")} className="font-medium text-xl border-b border-black px-2 py-2 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">About & Contact</div>
         <div onClick={() => onPush("/signup")} className="font-medium text-xl border-b border-black px-2 py-2 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">My Organisation</div>
