01cbe3ea288297b7072cc5466840191fe8dda3e7
dain <57316771+DainPark-web@users.noreply.github.com>
2024-08-20 10:02:59 +0200

    update

--- src/app/(home)/components/Menu.js
+++ src/app/(home)/components/Menu.js
@@ -8,7 +8,7 @@ const Menu = () => {
         router.push(path)
     }
   return (
-    <div className="h-full px-2 flex justify-center items-center cursor-pointer hover:bg-black hover:text-white transition-all relative z-[1300] group select-none font-jetBrainsMono font-medium border-r border-black stroke-black hover:stroke-white">
+    <div className="h-full px-2 flex justify-center items-center cursor-pointer hover:bg-black hover:text-white transition-all relative z-[1300] group select-none font-jetBrainsMono font-medium border-none border-black stroke-black hover:stroke-white">
       <MenuIcon style={{strokeWidth: ICON_STROKE_SIZE,width: ICON_SIZE, height: ICON_SIZE}} />
       <div className="hidden group-hover:flex flex-col absolute top-full left-0 h-fit bg-white border-y lg:border border-black text-black w-screen lg:w-fit">
         <div onClick={() => onPush("/about")} className="font-medium text-xl border-b border-black px-2 py-2 hover:bg-black hover:text-white transition-all whitespace-nowrap text-center lg:text-start">About & Contact</div>
