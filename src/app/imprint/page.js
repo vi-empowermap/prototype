15df623cbea8446f16090cc044a4be13c9847189
dain <57316771+DainPark-web@users.noreply.github.com>
2024-08-29 17:25:23 +0200

    update: set max length for intro text

--- src/app/imprint/page.js
+++ src/app/imprint/page.js
@@ -50,53 +50,10 @@ const getKirbyPanelData = async () => {
     return data
   };
 
-const getKirbyUsersData = async () => {
-  const bodyData = {
-    query: "kirby.users",
-    select: {
-      id: true,
-      orgaimage: true,
-      name: true,
-      email: true,
-      location: true,
-      city: true,
-      street: true,
-      zip: true,
-      contactnummber: true,
-      website: true,
-      social: true,
-      role_title: "user.role.title",
-      organame: true,
-      organameshortform: true,
-      aboutorga: true,
-      tags: true,
-      tagpool: true,
-      publicbtn: true,
-      lokalorga: true,
-      themenschwerpunkt: true,
-      artderorganisation: true,
-      zielgruppe: true,
-      onlineresourcen: true,
-      sprachunterstutzung: true,
-      angebote:true,
-      archivoraktiv: true,
-      bundesland: true,
-    },
-  };
-
-  /* KQL Fetch and get Data */
-  const kirbyApiDraft = `${kirbyOriginAPI}`;
-  const data = await fetchDataOriginAPI({ url: kirbyApiDraft, userInfo: { authEmail, authPassword }, method: "POST", bodyData });
-  
- 
-
-  return data
-
-}
 const Page = async () => {
     const kirbyPanelData = await getKirbyPanelData();
     const kirbyPanelHomeData = await getKirbyPanelHomeData();
-    const kirbyUsersList = await getKirbyUsersData()
+
 
  
     
