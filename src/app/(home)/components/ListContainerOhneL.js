3281474979b95b5e1ed020f63e9920555f79e695
Dain Park <zndgn555@gmail.com>
2024-10-07 17:37:06 +0200

    update

--- src/app/(home)/components/ListContainerOhneL.js
+++ src/app/(home)/components/ListContainerOhneL.js
@@ -1,6 +1,8 @@
+import {useEffect} from "react"
 import ListBoxOhneL from "./ListBoxOhneL";
 
 const ListContainerOhneL = ({ getData }) => {
+ 
   return (
     <div className={`w-full h-fit grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 auto-rows-auto p-2 gap-x-4 gap-y-4 pb-52`}>
       {/* {getData.length <= 0 && <div className="w-full text-center absolute left-0 top-1/2 -translate-y-1/2">No Data</div>} */}
