7d9d4be8744e7f6286ff235ec9acc1e4ccb093cf
Dain Park <zndgn555@gmail.com>
2024-10-10 12:36:22 +0200

    test: CSR

--- src/app/imprint/components/Wrapper.js
+++ src/app/imprint/components/Wrapper.js
@@ -2,12 +2,58 @@
 import { marked } from "marked";
 import MenuNav from "@/app/components/MenuNav";
 import { parseKirbyTags } from "@/app/utils/hooks/useParseKirbyTags";
+import useSWR from "swr";
+import { useEffect } from "react"
+import { fetchDataOriginAPI } from "@/app/utils/hooks/useFetchData";
+
+// Explore & Theme Page
+export const TestData = () => {
+const authEmail = process.env.KB_USER;
+const authPassword = process.env.KB_PASS;
+const kirbyOriginAPI = process.env.KB_API_ORIGIN;
+  const bodyData = {
+    query: "page('home')",
+    select: {
+      content:{
+        select: {
+          webtitle: true,
+         
+         
+
+        }
+      }
+    },
+   
+ }
+  const { data, error, isLoading } = useSWR(
+    // The key array will be passed to the fetcher as the argument
+    { url: kirbyOriginAPI, userInfo: { authEmail:authEmail, authPassword:authPassword }, method: "POST", bodyData:bodyData},  // This will be passed as the first argument to the fetcher
+    fetchDataOriginAPI   // The fetcher function
+  );
+
+  return {
+    data,
+    error,
+    isLoading
+  }
+
+
+}
+
+
 const Wrapper = ({
   kirbyPanelData: {
     result: { content: data },
   },
   kirbyPanelHomeData,
 }) => {
+
+  const {isLoading, data:dataSWR} = TestData()
+  useEffect(() => {
+    if(!isLoading){
+      console.log(dataSWR)
+    }
+  },[dataSWR])
  
   return (
     <div className="w-screen min-h-[100svh] lg:min-h-screen h-full bg-white relative pt-[44px] lg:pt-[86px] font-jetBrainsMonoLight">
